44a550fd2cdfd48a8c61371dae375fb3412d5430
Peter Kincaid <loaatao@gmail.com>
2017-10-28 20:42:16 -0500

    Completed Authentication

--- src/components/login/login.js
+++ src/components/login/login.js
@@ -1,29 +1,126 @@
 import React, { Component } from "react";
 
-import Register from "./register/register"
 import { Link } from "react-router-dom";
+
+import { fire, facebookProvider, twitterProvider } from "../../fire"
 
 export default class Login extends Component {
     constructor(props) {
         super(props);
+
+        this.state = {
+            authenticated: false
+        }
+
+        this.signOut = this.signOut.bind(this);
+        this.loginWithEmailPassword = this.loginWithEmailPassword.bind(this);
+        this.getAuthInfo = this.getAuthInfo.bind(this);
+        this.authWithFacebook = this.authWithFacebook.bind(this);
+    }
+
+    authWithFacebook() {
+        fire.auth().signInWithRedirect(facebookProvider)
+            .then((result, error) => {
+                console.log("logged in")
+                if (error) {
+                    console.log(error)
+                } else {
+                    console.log(this.state)
+                }
+            })
+    }
+
+    authWithTwitter() {
+        fire.auth().signInWithRedirect(twitterProvider)
+            .then((result, error) => {
+                if (error) {
+                    console.log(error);
+                } else {
+                    console.log("logged in");
+                }
+
+            })
+    }
+
+    loginWithEmailPassword(event) {
+        event.preventDefault();
+        const email = this.emailInput.value
+        const password = this.emailInput.value
+
+        fire.auth().signInWithEmailAndPassword(email, password)
+            .then(result => {
+                console.log(result)
+            })
+    }
+
+    getAuthInfo() {
+        fire.auth().onAuthStateChanged(user => {
+            if (user) {
+
+                console.log(user)
+            } else {
+
+                console.log("no user")
+            }
+        })
+    }
+
+    signOut() {
+        fire.auth().signOut().then(result => {
+
+            console.log(result, "logged out")
+        })
     }
 
     render() {
         return (
             <div>
+                <h1> Log In Page </h1>
 
-				<h1> Log In Page </h1>
+                     <div>
+                        <Link to ="/register">
+                        Register 
+                        </Link>
 
-				{this.props.authenticated
-					? <h3> Log Out </h3>
-					: <div>
-						<Link to ="/register">
-						Register 
-						</Link>
 
-					</div>
-				}
-			</div>
+            <button onClick={() => {this.authWithFacebook() }}> Login With Facebook </button>
+            <hr style={{marginTop: '10px', marginBottom:'10px'}}/>
+
+
+             <h3> Sign In With Your Email</h3>
+            <form onSubmit={(event) => { this.loginWithEmailPassword(event) }} ref={(form) => { this.loginForm = form }}>
+            Email
+            <input style={{width:"100%"}} name="email" type="email" ref={(input) => {this.emailInput = input}} placeholder="email"/>
+          <br/>
+          <br/>
+            Password
+            <input style={{width:"100%"}} name="password" type="password" ref={(input) => {this.passwordInput = input}} placeholder="password"/>
+          <br/>
+          <br/>
+          
+          <button onClick={(event) => {this.loginWithEmailPassword(event)}}>Log In </button>
+            </form>
+            <br/>
+            <br/>
+                <hr style={{marginTop: '10px', marginBottom:'10px'}}/>    
+                <br/>
+
+
+                        <button onClick={(event) => this.getAuthInfo()}> Get Auth Info</button>
+                        <br/>
+                        
+                        <button onClick={() => {this.authWithFacebook() }}> Login With Facebook </button>
+                        <br/>
+                        <button onClick={() => {this.authWithTwitter()}}>Login With Twitter</button>
+
+                        <br/>
+                        <button onClick= {(event) => this.signOut()}> Log Out </button>
+                        <br/>
+                        
+
+                    </div>
+            
+            </div>
         )
     }
 }
--- src/components/login/register/register.js
+++ src/components/login/register/register.js
@@ -1,22 +1,74 @@
 import React, { Component } from "react";
 
+
+import { fire, facebookProvider } from "../../../fire";
+
 import "./register.css";
 
 export default class Register extends Component {
+    constructor(props) {
+        super(props);
+
+        
+        this.state = {
+            authenticated: false
+        }
+    }
+
+
+    authWithFacebook() {
+        fire.auth().signInWithRedirect(facebookProvider)
+            .then((result, error) => {
+                if (error) {
+                    console.log(error)
+                } else {
+                    this.setState({ redirect: true})
+                    console.log(this.state)
+                }
+            })
+    }
+
+    authWithEmailPassword(event) {
+        event.preventDefault()
+        const email = this.emailInput.value
+        const password = this.emailInput.value
+
+        fire.auth().createUserWithEmailAndPassword(email, password).then(result => {
+          console.log(result)
+        })
+        .catch(error => {
+          console.log(error)
+        })
+    }
+
     render() {
+
         return (
             <div>
-        		<h1> Sign Up For An Account </h1>
+            <h1> Register Page </h1>
+           
+           <h3> Sign Up With Facebook </h3>
 
-        		<h3> Name </h3>
-        		<input className="registerInputBorder" type="text" />
+            <button onClick={() => {this.authWithFacebook() }}> Login With Facebook </button>
+            <hr style={{marginTop: '10px', marginBottom:'10px'}}/>
 
-        		<h3> Email </h3>
-        		<input className="registerInputBorder" type="email" /> 
 
-        		<h3> Password </h3>
-        		<input className="registerInputBorder" type="password" />
-        	</div>
+             <h1> Sign Up With Your Email</h1>
+            <form onSubmit={(event) => { this.authWithEmailPassword(event) }} ref={(form) => { this.loginForm = form }}>
+            Email
+            <input style={{width:"100%"}} name="email" type="email" ref={(input) => {this.emailInput = input}} placeholder="email"/>
+          
+          <br/>
+          <br/>
+          <br/>
+          
+            Password
+            <input style={{width:"100%"}} name="password" type="password" ref={(input) => {this.passwordInput = input}} placeholder="password"/>
+          <br/>
+
+          <button onClick={(event) => {this.authWithEmailPassword(event)}}> Create Account </button>
+            </form>
+          </div>
         );
     }
 }
