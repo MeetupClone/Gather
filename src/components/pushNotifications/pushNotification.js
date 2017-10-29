8bd730097da594350507d9adb24bbe593fcde516
Peter Kincaid <loaatao@gmail.com>
2017-10-28 23:36:38 -0500

    Started work on push notifications

---
+++ src/components/pushNotifications/pushNotification.js
@@ -0,0 +1,16 @@
+import React, {Component} from "react";
+
+
+// import {fire as firebase} from "../../fire";
+
+
+export default class PushNotifications extends Component {
+	render() {
+		return(
+			<div>
+
+			</div>
+			)
+	}
+
+}
