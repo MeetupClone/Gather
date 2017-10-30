544a25875cb159ebe2eed7040b10ddceb61a5573
Peter Kincaid <loaatao@gmail.com>
2017-10-29 20:18:43 -0500

    Update config.js

--- server/keys/config.js
+++ server/keys/config.js
@@ -1,3 +1,1 @@
-module.exports = {
-	herokuDb: "postgres://wmczpjyuxlvfez:aedd722cd8435a398630ddfc15c24ea022bd80a7d48439e6403263afb55f465a@ec2-23-23-248-247.compute-1.amazonaws.com/db5lrijnrnsu9"
-}
+
