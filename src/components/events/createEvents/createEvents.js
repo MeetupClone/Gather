324c4ddf595fb6c2e58d17059b5dda465d334c03
Chuck <chuckcipione@yahoo.com>
2017-10-31 10:54:06 -0500

    poop

--- src/components/events/createEvents/createEvents.js
+++ src/components/events/createEvents/createEvents.js
@@ -1,12 +1,36 @@
 import React, {Component} from 'react';
+import { connect } from 'react-redux';
+
+export default class CreateEvent extends Component{
+  constructor(props){
+  super(props)
 
 
-export default class CreateEvents extends Component{
-    render(){
+this.state = {
+  eventPic: '',
+  eventName: '',
+  uid: '',
+  description: '',
+  location: '',
+  category: ''  
+}
+}
+
+ 
+   render(){
+      const {eventPic, eventName, uid, description, location, category} = this.props
         return(
             <div>
-                <h1>TEST</h1>
-            </div>
-        )
-    }
-}
+              <form>
+                <input  type={text} ref={(input)=>{this.eventName = input}}/>
+                  <input name=eventPic type=text ref=(input)=>{this.eventPic = input}/>
+
+                   <input name=description type=text ref=(input)=>{this.description = input}/>
+                      <input name=location type=location ref=(input)=>{this.location=input}/>
+                        <input name=category type=category ref=(input)=>{this.category=input}/>
+                        </form>
+                      </div>
+                      }
+                      )
+
+                     }
