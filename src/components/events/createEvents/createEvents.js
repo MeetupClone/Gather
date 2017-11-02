bb56b16ef26776723fb6841509a0a1492260d5ac
Chuck <chuckcipione@yahoo.com>
2017-11-02 09:34:39 -0500

    ignore

--- src/components/events/createEvents/createEvents.js
+++ src/components/events/createEvents/createEvents.js
@@ -1,22 +1,12 @@
 import React, {Component} from 'react';
 import { connect } from 'react-redux';
-<<<<<<< HEAD
-=======
 import redux from 'react-redux';
 
->>>>>>> 0e287130ca5c2d031e0167f06182241b2c197b02
 
 export default class CreateEvent extends Component{
   constructor(props){
   super(props)
 
-<<<<<<< HEAD
-=======
-export default class CreateEvents extends Component{
-  constructor(props){
-  super(props)
-
->>>>>>> 0e287130ca5c2d031e0167f06182241b2c197b02
 
 this.state = {
   eventPic: '',
@@ -24,30 +14,6 @@ this.state = {
   uid: '',
   description: '',
   location: '',
-<<<<<<< HEAD
-  category: ''  
-}
-}
-
- 
-   render(){
-      const {eventPic, eventName, uid, description, location, category} = this.props
-        return(
-            <div>
-              <form>
-                <input  type={text} ref={(input)=>{this.eventName = input}}/>
-                  <input name=eventPic type=text ref=(input)=>{this.eventPic = input}/>
-
-                   <input name=description type=text ref=(input)=>{this.description = input}/>
-                      <input name=location type=location ref=(input)=>{this.location=input}/>
-                        <input name=category type=category ref=(input)=>{this.category=input}/>
-                        </form>
-                      </div>
-                      }
-                      )
-
-                     }
-=======
   category: ''
 }
 }
@@ -77,4 +43,3 @@ this.state = {
 // const actions = {eventPic, eventName, uid, description, location, category}
 
 // export default connect(mapStateToProps, actions)(CreateEvent)
->>>>>>> 0e287130ca5c2d031e0167f06182241b2c197b02
