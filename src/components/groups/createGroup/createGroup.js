f687ebca6bf4abd4aebd3ec2aa1364c2fe591567
Shiva Patel <shivapatel15@yahoo.com>
2017-11-02 16:55:30 -0400

    fixed bug of creategroup

--- src/components/groups/createGroup/createGroup.js
+++ src/components/groups/createGroup/createGroup.js
@@ -1,7 +1,8 @@
 import React, {Component} from 'react';
 import { connect } from 'react-redux';
+import {createGroup} from "../../../ducks/create-group-redux"
 
-export default class CreateGroup extends Component{
+export class CreateGroup extends Component{
     constructor(props){
         super(props)
 
@@ -23,6 +24,7 @@ export default class CreateGroup extends Component{
     }
 
     render(){
+      const {createGroup} = this.props
         let confirmModalElement = null
         if (this.state.confirmModal === true){
             confirmModalElement = (<h1>You made a group!</h1>)
@@ -30,24 +32,24 @@ export default class CreateGroup extends Component{
         }
         return(
             <div>
-                {confirmModalElement}
-                <h1>Create Group</h1>
-                <form>
-                    <input type="text" placeholder="Name" onChange={e=>this.handleChange(e.target.value, "name")} ref={(input) => {this.name = input}}></input>
-                    <input type="text" placeholder="Category" onChange={e=>this.handleChange(e.target.value, "category")} ref={(input) => {this.category = input}}></input>
-                    <input type="text" placeholder="Description" onChange={e=>this.handleChange(e.target.value, "description")} ref={(input) => {this.description = input}}></input>
-                    <input type="text" placeholder="Website" onChange={e=>this.handleChange(e.target.value, "website")} ref={(input) => {this.website = input}}></input>
-                    <input type="text" placeholder="Twitter" onChange={e=>this.handleChange(e.target.value, "twitter")} ref={(input) => {this.twitter = input}}></input>
-                    <input type="text" placeholder="Facebook" onChange={e=>this.handleChange(e.target.value, "facebook")} ref={(input) => {this.facebook = input}}></input>
-                    <input type="text" placeholder="Instagram" onChange={e=>this.handleChange(e.target.value, "instagram")} ref={(input) => {this.instagram = input}}></input>
-                </form>
-                    <button onClick={(event) => {
-                        console.log("state", this.state)
-                        event.preventDefault()
-                        createGroup(this.state).then(result => {
-                            if(result === true){
-                                this.setState({confirmModal: true})
-                            }
+              {confirmModalElement}
+              <h1>Create Group</h1>
+              <form>
+                <input type="text" placeholder="Name" onChange={e=>this.handleChange(e.target.value, "name")} ref={(input) => {this.name = input}}></input>
+                <input type="text" placeholder="Category" onChange={e=>this.handleChange(e.target.value, "category")} ref={(input) => {this.category = input}}></input>
+                <input type="text" placeholder="Description" onChange={e=>this.handleChange(e.target.value, "description")} ref={(input) => {this.description = input}}></input>
+                <input type="text" placeholder="Website" onChange={e=>this.handleChange(e.target.value, "website")} ref={(input) => {this.website = input}}></input>
+                <input type="text" placeholder="Twitter" onChange={e=>this.handleChange(e.target.value, "twitter")} ref={(input) => {this.twitter = input}}></input>
+                <input type="text" placeholder="Facebook" onChange={e=>this.handleChange(e.target.value, "facebook")} ref={(input) => {this.facebook = input}}></input>
+                <input type="text" placeholder="Instagram" onChange={e=>this.handleChange(e.target.value, "instagram")} ref={(input) => {this.instagram = input}}></input>
+              </form>
+              <button onClick={(event) => {
+                console.log("state", this.state)
+                event.preventDefault()
+                createGroup(this.state).then(result => {
+                  if(result === true){
+                    this.setState({confirmModal: true})
+                  }
                         })
                     }}>Submit</button>
             </div>
@@ -55,10 +57,12 @@ export default class CreateGroup extends Component{
     }
 }
 
-const mapStateToProps = (state) => { return state }
+
 
 const mapStateToProps = (state) => { return {} }
 
 const actions = {
     createGroup
 }
+
+export default connect(mapStateToProps, actions)(CreateGroup)
