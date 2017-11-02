57f105839aeac4b3c7da6dac3017e08951c36d8f
Shiva Patel <shivapatel15@yahoo.com>
2017-11-02 18:02:49 -0400

    footer made

--- src/components/home/notAuthHome/notAuthHome.js
+++ src/components/home/notAuthHome/notAuthHome.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 
 import './notAuthHome.css';
-
+import {Footer} from '../../events/footer/footer'
 export default class NotAuthHome extends Component{
     constructor(props){
         super(props)
@@ -13,28 +13,28 @@ export default class NotAuthHome extends Component{
     render(){
         return(
             <div>
+              <div>
+                <img className="gather-logo-home" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRIeLhOTEqGQr6dhj8-s3MumDuNon40G0srsYOGaPgH1glxDFuLaA" alt="gather logo"/>
+                <h3>gath·er <br/> ˈɡaT͟Hər <br/> verb <br/> 1. <br/> come together; assemble or accumulate.</h3>
+              </div>
+              <div className="home-event-card">
+                <img className="home-event-card-pic" src="https://petapixel.com/assets/uploads/2015/03/iStock-Unfinished-Business-5.jpg" alt="event pic"/>
+                <div >
+                  <h2>Explore</h2>
+                  <p> Blah Blah Blah</p>
+                  <button> Someone Link Me to the events page</button>
+                </div>
+              </div>
+              <div className="home-create-card">
                 <div>
-                    <img className="gather-logo-home" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRIeLhOTEqGQr6dhj8-s3MumDuNon40G0srsYOGaPgH1glxDFuLaA" alt="gather logo"/>
-                    <h3>gath·er <br/> ˈɡaT͟Hər <br/> verb <br/> 1. <br/> come together; assemble or accumulate.</h3>
+                  <h2>Create</h2>
+                  <p> Blah Blah Blah</p>
+                  <button> Someone Link Me to the login/register page</button>
                 </div>
-                <div className="home-event-card">
-                    <img className="home-event-card-pic" src="https://petapixel.com/assets/uploads/2015/03/iStock-Unfinished-Business-5.jpg" alt="event pic"/>
-                    <div >
-                        <h2>Explore</h2>
-                        <p> Blah Blah Blah</p>
-                        <button> Someone Link Me to the events page</button>
-                    </div>
-                </div>
-                <div className="home-create-card">
-                    <div>
-                        <h2>Create</h2>
-                        <p> Blah Blah Blah</p>
-                        <button> Someone Link Me to the login/register page</button>
-                    </div>
-                    <img className="home-create-card-pic" src="https://petapixel.com/assets/uploads/2015/03/iStock-Unfinished-Business-5.jpg" alt="create pic"/>
+                <img className="home-create-card-pic" src="https://petapixel.com/assets/uploads/2015/03/iStock-Unfinished-Business-5.jpg" alt="create pic"/>
 
-                </div>
-                Footer
+              </div>
+              <Footer/>
             </div>
         )
     }
