5080192cd641b7265d99aa84892eb2b709ec1417
Peter Kincaid <loaatao@gmail.com>
2017-11-07 11:46:47 -0600

    bugs

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
 import { Link } from "react-router-dom";
 
-import { fire as firebase } from "../../fire"
+import { fire as firebase } from "../../fire";
+
+import "./footer.css";
 
 export default class Footer extends Component {
     constructor(props) {
@@ -11,7 +13,7 @@ export default class Footer extends Component {
             user: false,
         }
 
-        
+
 
     }
 
@@ -28,13 +30,31 @@ export default class Footer extends Component {
 
 
     render() {
+        if (this.state.user) {
+            return (
+                <div>
+        <Link to="/event/create">
+      <button className="footer-create-button">Create</button>
+        </Link>
+      <div className="footer-background">
+      <h5 className="footer-links"><Link to="/" className="footer-home">Home </Link><Link to="/faq"> About </Link><span className="footer-home"> Settings</span></h5>
+      <h6 className="footer-milk-steak"> Milk Steak LLC</h6>
+    </div>
+    </div>
+            )
+        } else {
+            return (
+                <div>
+        <Link to="/login">
+      <button className="footer-create-button">Create</button>
+        </Link>
+      <div className="footer-background">
+      <h5 className="footer-links"><Link to="/" className="footer-home">Home </Link><Link to="/faq"> About </Link><Link to="/user/account" className="footer-home"> Settings</Link></h5>
+      <h6 className="footer-milk-steak"> Milk Steak LLC</h6>
+    </div>
+    </div>
+            )
+        }
 
-        return (
-            <div>
-        <Link to = "/event/create"><h4>Create</h4></Link>
-        <h5><Link to = "/"><span>Home </span></Link><Link to = ""><span> About </span></Link><Link to = "/user/account"><span> Settings</span></Link></h5>
-        <h6> This is for educational purposes</h6>
-      </div>
-        )
     }
 }
