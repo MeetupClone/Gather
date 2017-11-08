4a3ea7802772fd2c87ac68f25383ad05c03a90ab
Shiva Patel <shivapatel15@yahoo.com>
2017-11-08 16:50:01 -0500

    added sql query for join

--- db/getRelevantEvents.sql
+++ db/getRelevantEvents.sql
@@ -1,3 +1,11 @@
-select * from events
-  JOIN user_categories ON events.category = user_categories.category
-  WHERE user_categories.user_id = $1
+
+ 
+   SELECT * FROM user_categories
+  JOIN category_events ON user_categories.category = category_events.category_name
+  JOIN events ON events.id = category_events.event_id
+  WHERE user_categories.user_id != events.organizer_uid
+
+  
+  
+  -- id of the event
+  -- userid
