2ea2d3d3eea9c7eaf008f54946463dae70498bd7
Peter Kincaid <loaatao@gmail.com>
2017-11-10 11:12:11 -0600

    fixed master

--- src/store.js
+++ src/store.js
@@ -4,9 +4,10 @@ import { devToolsEnhancer } from 'redux-devtools-extension';
 import AuthenticationReducer from './ducks/authentication-redux'
 import EventReducer from './ducks/event-redux'
 import GroupReducer from './ducks/group-redux'
+import CommentReducer from './ducks/comment-redux'
 import promiseMiddleware from "redux-promise-middleware"
 
-let store = createStore(combineReducers({ AuthenticationReducer, EventReducer, GroupReducer }),
+let store = createStore(combineReducers({ AuthenticationReducer, EventReducer, GroupReducer, CommentReducer }),
     devToolsEnhancer(),
     applyMiddleware(promiseMiddleware())
 );
