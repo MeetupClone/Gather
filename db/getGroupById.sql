b667bc1fdee21949465dd79d7111f61d385660ee
Mackenzie <clarkmackenzieb@gmail.com>
2017-11-10 12:05:04 -0600

    SORRY forgot to feed in parameters instead of test value
    this endpoint should be a-ok now sorry

--- db/getGroupById.sql
+++ db/getGroupById.sql
@@ -1,4 +1,4 @@
 SELECT *, COUNT(groups_members.user_id) as members FROM groups
   JOIN groups_members ON groups_members.group_id = groups.id
-  WHERE groups.id = 2
+  WHERE groups.id = $1
   GROUP BY groups.id, groups_members.id;
