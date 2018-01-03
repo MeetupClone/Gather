cc290e152381d2965461ec8647b625fc301410ef
Peter Miles <hello@petermiles.io>
2018-01-03 12:43:06 -0600

    Saved Button Styling

--- src/components/login/register/register.js
+++ src/components/login/register/register.js
@@ -5,81 +5,106 @@ import { connect } from "react-redux";
 import { authWithEmailPassword, authWithFacebook } from "../../../ducks/authentication-redux";
 
 import "../login.css";
-import Category from "../../categories/category"
+import Category from "../../categories/category";
 
 export class Register extends Component {
     constructor(props) {
         super(props);
 
-
         this.state = {
-            email: '',
-            password: '',
-            name: '',
+            email: "",
+            password: "",
+            name: "",
             authenticated: false,
             categoriesAdded: false,
-            categories: []
-        }
+            categories: [],
+        };
 
-        this.updateParent = (state) => this.props.updateParent(this.state)
+        this.updateParent = state => this.props.updateParent(this.state);
     }
 
     render() {
-        let createButtons = null
+        let createButtons = null;
 
         if (this.state.categoriesAdded) {
             createButtons = (
                 <div>
-                <button className="login-button box-shadow" onClick={(event) => {
-                event.preventDefault()
-                authWithEmailPassword(this.state)}}> Create Account </button>
-<div id="providers-auth" className="center">
-                            <button className="auth-button facebook box-shadow" onClick={(event)=> {
+                    <button
+                        className="login-button box-shadow"
+                        onClick={event => {
+                            event.preventDefault();
+                            authWithEmailPassword(this.state);
+                        }}
+                    >
+                        {" "}
+                        Create Account{" "}
+                    </button>
+                    <div id="providers-auth" className="center">
+                        <button
+                            className="auth-button facebook box-shadow"
+                            onClick={event => {
                                 event.preventDefault();
-                                authWithFacebook(this.state.categories) }}>
-                                <img className="auth-icon" src={require( "../assets/facebook.svg")} alt="facebook" /> Sign Up With Facebook </button>
-                        </div>
-                        </div>
-           )
+                                authWithFacebook(this.state.categories);
+                            }}
+                        >
+                            <img className="auth-icon" src={require("../assets/facebook.svg")} alt="facebook" /> Sign Up With
+                            Facebook{" "}
+                        </button>
+                    </div>
+                </div>
+            );
         } else {
             createButtons = (
                 <div>
-                <h1> Choose some categories! </h1>
-                <h3>These allow us to provide you with events that you may like </h3>
-                <h6> You can change these any time in your account settings </h6>
+                    <h1> Choose some categories! </h1>
+                    <h3>These allow us to provide you with events that you may like </h3>
+                    <h6> You can change these any time in your account settings </h6>
                 </div>
-                )
+            );
         }
-    
-        const {
-            authWithEmailPassword,
-            authWithFacebook,
-        } = this.props
+
+        const { authWithEmailPassword, authWithFacebook } = this.props;
         return (
             <div id="register-page">
-            <h1 id="register-title"> Register </h1> 
+                <h1 id="register-title"> Register </h1>
 
-                <input name="name" type="name" onChange={(event) => this.setState({name: event.target.value})} placeholder="Name"/>
-                <input name="email" type="email" onChange={(event) => this.setState({email : event.target.value})} placeholder="Email"/>
-                <input name="password" type="password" onChange={(event) => this.setState({password: event.target.value})} placeholder="Password"/>
-                <Category className="categorySelector" updateParent={(state) => {
-                this.setState({categoriesAdded : true, categories: state})}}/>
+                <input
+                    name="name"
+                    type="name"
+                    onChange={event => this.setState({ name: event.target.value })}
+                    placeholder="Name"
+                />
+                <input
+                    name="email"
+                    type="email"
+                    onChange={event => this.setState({ email: event.target.value })}
+                    placeholder="Email"
+                />
+                <input
+                    name="password"
+                    type="password"
+                    onChange={event => this.setState({ password: event.target.value })}
+                    placeholder="Password"
+                />
+                <Category
+                    className="categorySelector"
+                    updateParent={state => {
+                        this.setState({ categoriesAdded: true, categories: state });
+                    }}
+                />
                 {createButtons}
-          </div>
+            </div>
         );
     }
 }
 
-
-const mapStateToProps = (state) => {
-    return {}
-}
-
-
+const mapStateToProps = state => {
+    return {};
+};
 
 const actions = {
     authWithEmailPassword,
-    authWithFacebook
-}
+    authWithFacebook,
+};
 
 export default connect(mapStateToProps, actions)(Register);
--- src/ducks/authentication-redux.js
+++ src/ducks/authentication-redux.js
@@ -1,145 +1,163 @@
-import { fire as firebase, facebookProvider } from "../fire"
-import axios from 'axios';
-
+import { fire as firebase, facebookProvider } from "../fire";
+import axios from "axios";
 
 //INITIAL STATE OF DATA - IF NONE, EMPTY STRING, EMPTY ARRAY OR WHATEVER. FILLER SHIT.
 const initialState = {
-    uid: '',
-    email: '',
-    authenticated: false
-}
+    uid: "",
+    email: "",
+    authenticated: false,
+};
 firebase.auth().onAuthStateChanged(user => {
     if (user) {
-        initialState.uid = user.uid
+        initialState.uid = user.uid;
     }
-})
+});
 
 //CONSTANTS
-const AUTH_WITH_FACEBOOK = 'AUTH_WITH_FACEBOOK';
-const AUTH_WITH_EMAIL_PASSWORD = 'AUTH_WITH_EMAIL_PASSWORD'
-const LOGIN_WITH_EMAIL_PASSWORD = 'LOGIN_WITH_EMAIL_PASSWORD';
-const GET_AUTH_INFO = 'GET_AUTH_INFO';
-const REGISTER_FCM_KEY = 'REGISTER_FCM_KEY'
-const SIGN_OUT = 'SIGN_OUT';
-
+const AUTH_WITH_FACEBOOK = "AUTH_WITH_FACEBOOK";
+const AUTH_WITH_EMAIL_PASSWORD = "AUTH_WITH_EMAIL_PASSWORD";
+const LOGIN_WITH_EMAIL_PASSWORD = "LOGIN_WITH_EMAIL_PASSWORD";
+const GET_AUTH_INFO = "GET_AUTH_INFO";
+const REGISTER_FCM_KEY = "REGISTER_FCM_KEY";
+const SIGN_OUT = "SIGN_OUT";
 
 //ACTION BUILDERS REMEMBER TO EXPORT THEM
 
 export function authWithFacebook(categories) {
     return {
         type: AUTH_WITH_FACEBOOK,
-        payload: 
-        firebase.auth().signInWithPopup(facebookProvider)
+        payload: firebase
+            .auth()
+            .signInWithPopup(facebookProvider)
             .then(result => {
-                console.log(result)
+                console.log(result);
                 if (result.additionalUserInfo.isNewUser) {
-                    console.log(result, "from redux")
-                    return axios.post('/api/user/createUser', [result.user.uid, result.user.email, result.user.displayName, result.user.photoURL, categories]).then(result => {
-                        return true
-                    })
+                    console.log(result, "from redux");
+                    return axios
+                        .post("/api/user/createUser", [
+                            result.user.uid,
+                            result.user.email,
+                            result.user.displayName,
+                            result.user.photoURL,
+                            categories,
+                        ])
+                        .then(result => {
+                            return true;
+                        });
                 } else {
-                    axios.post('/api/user/createUser', [result.user.uid, result.user.email, result.user.displayName, result.user.photoURL, categories]).then(result => {
-                        return true
-                    })
+                    axios
+                        .post("/api/user/createUser", [
+                            result.user.uid,
+                            result.user.email,
+                            result.user.displayName,
+                            result.user.photoURL,
+                            categories,
+                        ])
+                        .then(result => {
+                            return true;
+                        });
                 }
-                return result.user
-
-            })
-    }
+                return result.user;
+            }),
+    };
 }
 
 export function authWithEmailPassword(initialState) {
     return {
         type: AUTH_WITH_EMAIL_PASSWORD,
-        payload: initialState
-    }
+        payload: initialState,
+    };
 }
 
 export function loginWithEmailPassword(initialState) {
-    console.log(initialState)
+    console.log(initialState);
     return {
         type: LOGIN_WITH_EMAIL_PASSWORD,
-        payload: firebase.auth().signInWithEmailAndPassword(initialState.email, initialState.password)
+        payload: firebase
+            .auth()
+            .signInWithEmailAndPassword(initialState.email, initialState.password)
             .then(user => {
-                localStorage.setItem('uid', user.uid)
-                return user
-            })
-    }
+                localStorage.setItem("uid", user.uid);
+                return user;
+            }),
+    };
 }
 
 export function getAuthInfo(initialState) {
     return {
         type: GET_AUTH_INFO,
-        payload: firebase.auth().onAuthStateChanged(user => { return user })
-    }
+        payload: firebase.auth().onAuthStateChanged(user => {
+            return user;
+        }),
+    };
 }
 
 export function signOut() {
     return {
-        type: SIGN_OUT
-    }
+        type: SIGN_OUT,
+    };
 }
-
-export function registerFcmKey(initialState) {
-    return {
-        type: REGISTER_FCM_KEY,
-        initialState
-    }
-}
-
 
 export default function AuthenticationReducer(state = initialState, action) {
     switch (action.type) {
-
         case AUTH_WITH_EMAIL_PASSWORD:
-            let emailValue = action.payload.email
-            let passwordValue = action.payload.password
-            let nameValue = action.payload.name
-            firebase.auth().createUserWithEmailAndPassword(emailValue, passwordValue).then(user => {
-                localStorage.setItem('loggedIn', true)
-                axios.post('/api/user/createUser', [user.uid, user.email, nameValue, 'https://firebasestorage.googleapis.com/v0/b/gatherv0-b3651.appspot.com/o/defaultPic.webp?alt=media&token=73d67fbf-6f0e-40aa-8fc9-15ec9e8e4fd9'])
-                console.log(user)
-                return Object.assign({}, state, {
-                    uid: user.uid,
-                    email: user.email,
-                    authenticated: true
-                })
-            })
-            return Object.assign({}, state, { authenticated: true })
+            let emailValue = action.payload.email;
+            let passwordValue = action.payload.password;
+            let nameValue = action.payload.name;
+            firebase
+                .auth()
+                .createUserWithEmailAndPassword(emailValue, passwordValue)
+                .then(user => {
+                    localStorage.setItem("loggedIn", true);
+                    axios.post("/api/user/createUser", [
+                        user.uid,
+                        user.email,
+                        nameValue,
+                        "https://firebasestorage.googleapis.com/v0/b/gatherv0-b3651.appspot.com/o/defaultPic.webp?alt=media&token=73d67fbf-6f0e-40aa-8fc9-15ec9e8e4fd9",
+                    ]);
+                    console.log(user);
+                    return Object.assign({}, state, {
+                        uid: user.uid,
+                        email: user.email,
+                        authenticated: true,
+                    });
+                });
+            return Object.assign({}, state, { authenticated: true });
         case AUTH_WITH_FACEBOOK + "_PENDING":
-            return Object.assign({}, state, { authenticated: false })
+            return Object.assign({}, state, { authenticated: false });
         case AUTH_WITH_FACEBOOK + "_FULFILLED":
-            let user = action.payload
-            return { user, authenticated: true }
+            let user = action.payload;
+            return { user, authenticated: true };
         case LOGIN_WITH_EMAIL_PASSWORD + "_PENDING":
-            return Object.assign({}, state, { authenticated: false })
+            return Object.assign({}, state, { authenticated: false });
         case LOGIN_WITH_EMAIL_PASSWORD + "_FULFILLED":
-            let userInfo = action.payload
-            return { userInfo: userInfo, authenticated: true }
+            let userInfo = action.payload;
+            return { userInfo: userInfo, authenticated: true };
         case GET_AUTH_INFO + "_PENDING":
-            return Object.assign({}, state, { authenticated: false })
+            return Object.assign({}, state, { authenticated: false });
         case GET_AUTH_INFO + "_FULFILLED":
-            let userInfo2 = action.payload
-            return { userInfo2, authenticated: true }
+            let userInfo2 = action.payload;
+            return { userInfo2, authenticated: true };
         case REGISTER_FCM_KEY:
-            const messaging = firebase.messaging()
+            const messaging = firebase.messaging();
             messaging.requestPermission().then(result => {
-                console.log("have permission")
-                return messaging.getToken()
-                    .then(token => {
-                        console.log(token)
-                    })
-            })
+                console.log("have permission");
+                return messaging.getToken().then(token => {
+                    console.log(token);
+                });
+            });
             return state;
         case SIGN_OUT:
-            firebase.auth().signOut().then(result => {
-                return Object.assign({}, state, {
-                    authenticated: false
-                })
-            })
+            firebase
+                .auth()
+                .signOut()
+                .then(result => {
+                    return Object.assign({}, state, {
+                        authenticated: false,
+                    });
+                });
             break;
         default:
             return state;
     }
-
 }
