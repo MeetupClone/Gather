161ba525e967b0aec767586135bef01f180bb761
Peter Miles <hello@petermiles.io>
2018-02-06 21:53:40 -0600

    convert stateful components to stateless components. parent is now singleton of data

--- src/components/events/explore/exploreSearch/eventCards/eventCards.js
+++ src/components/events/explore/exploreSearch/eventCards/eventCards.js
@@ -1,75 +1,37 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 import { Link } from 'react-router-dom';
-import axios from 'axios';
 
 import './eventCards2.css';
 import 'helpers.css';
 
-export class EventCards extends Component {
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            events: [],
-            searchFilter: 'name',
-            searchText: '',
-            searchField: 1,
-            loading: true,
-        };
-
-        this.handleSearch = this.handleSearch.bind(this);
-    }
-
-    componentDidMount() {
-        axios
-            .get('/api/events')
-            .then(result => {
-                this.setState({
-                    loading: false,
-                    events: result.data.sort((a, b) => {
-                        return (
-                            new Date(b.cron_time).getTime() -
-                            new Date(a.cron_time).getTime()
-                        );
-                    }),
-                });
-            })
-            .catch(() => this.setState({ loading: false }));
-    }
-
-    handleSearch() {}
-
-    render() {
-        return this.state.loading ? (
-            <div>
-                {Array.apply(null, { length: 4 }).map((x, i) => {
-                    return (
-                        <div className="event-card-loading-container" key={i} />
-                    );
-                })}
-            </div>
-        ) : (
-            <div>
-                {this.props.events.map((event, i) => {
-                    return (
-                        <Link key={i} to={`/event/${event.id}`}>
-                            <div className="card-container">
-                                <p>{event.title}</p>
-                                <p className="event-card-desc">
-                                    {event.description}
-                                </p>
-                                <p className="event-card-date nunito-text">
-                                    {event.event_date}
-                                </p>
-                                <p className="event-card-loc">
-                                    {event.location.toUpperCase()}
-                                </p>
-                            </div>
-                        </Link>
-                    );
-                })}
-            </div>
-        );
-    }
-}
+export const EventCards = props => {
+    return props.loading ? (
+        <div>
+            {Array.apply(null, { length: 4 }).map((x, i) => {
+                return <div className="event-card-loading-container" key={i} />;
+            })}
+        </div>
+    ) : (
+        <div>
+            {props.events.map((event, i) => {
+                return (
+                    <Link key={i} to={`/event/${event.id}`}>
+                        <div className="card-container">
+                            <p>{event.title}</p>
+                            <p className="event-card-desc">
+                                {event.description}
+                            </p>
+                            <p className="event-card-date nunito-text">
+                                {event.event_date}
+                            </p>
+                            <p className="event-card-loc">
+                                {event.location.toUpperCase()}
+                            </p>
+                        </div>
+                    </Link>
+                );
+            })}
+        </div>
+    );
+};
--- src/components/events/explore/exploreSearch/groupCards/groupCards.js
+++ src/components/events/explore/exploreSearch/groupCards/groupCards.js
@@ -1,84 +1,43 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
-import axios from 'axios';
 
 import './groupCards.css';
 import 'helpers.css';
 
-export class GroupCards extends Component {
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            groups: [],
-            searchFilter: 'name',
-            searchText: '',
-            searchField: 1,
-            loading: true,
-        };
-
-        this.componentWillMount = this.componentWillMount.bind(this);
-    }
-
-    componentWillReceiveProps(props) {
-        this.setState({
-            searchFilter: props.searchFilter,
-            searchText: props.searchText,
-        });
-    }
-
-    componentWillMount() {
-        axios.get('/api/groups').then(result => {
-            this.setState({ loading: false, groups: result.data });
-        });
-    }
-    render() {
-        return this.props.loading ? (
-            <div>
-                {Array.apply(null, { length: 4 }).map((x, i) => {
-                    return (
-                        <div className="event-card-loading-container" key={i} />
-                    );
-                })}
-            </div>
-        ) : (
-            <div>
-                {this.props.groups.map((group, i) => {
-                    console.log(group);
-                    return (
-                        <Link key={i} to={`/groups/${group.id}`}>
-                            <div className="card-container">
+export const GroupCards = props => {
+    return props.loading ? (
+        <div>
+            {Array.apply(null, { length: 4 }).map((x, i) => {
+                return <div className="event-card-loading-container" key={i} />;
+            })}
+        </div>
+    ) : (
+        <div>
+            {props.groups.map((group, i) => {
+                return (
+                    <Link key={i} to={`/groups/${group.id}`}>
+                        <div className="group-card-container">
+                            {group.group_picture ? (
                                 <img
                                     className="group-card-image"
-                                    src={
-                                        group.group_picture ||
-                                        require(`../../../../../web-p-category-pics/${
-                                            group.category
-                                        }.webp`)
-                                    }
+                                    src={group.group_picture}
                                     alt={group.name}
                                 />
-                                <div className="group-card-content">
-                                    <div>
-                                        <p className="group-card-name">
-                                            {group.name}
-                                        </p>
-                                    </div>
-                                    <div>
-                                        {group.category
-                                            .charAt(0)
-                                            .toUpperCase() +
-                                            group.category.slice(1)}
-                                    </div>
-                                    <div>
-                                        {group.number_of_members} Member(s)
-                                    </div>
+                            ) : null}
+
+                            <div className="group-card-content">
+                                <p className="group-card-name">{group.name}</p>
+
+                                <div>
+                                    {group.category.charAt(0).toUpperCase() +
+                                        group.category.slice(1)}
                                 </div>
+                                <div>{group.number_of_members} Member(s)</div>
                             </div>
-                        </Link>
-                    );
-                })}
-            </div>
-        );
-    }
-}
+                        </div>
+                    </Link>
+                );
+            })}
+        </div>
+    );
+};
